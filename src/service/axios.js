684afa731a98e8e36310d430308400843fa0df8c
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2020-11-01 21:33:59 +0800

    deployed!!

--- src/service/axios.js
+++ src/service/axios.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 // the baseURL is the API (cloud function)
 const instance = axios.create({
-	baseURL: "http://localhost:5001/clone-v2-c67f8/us-central1/api",
+	// baseURL: "http://localhost:5001/clone-v2-c67f8/us-central1/api",
+	baseURL: "https://us-central1-clone-v2-c67f8.cloudfunctions.net/api",
 });
 
 export default instance;
